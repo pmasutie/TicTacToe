ed8f475f1fee65da10696b702a1eedbea970efce
Benjamin Rae <benjaminrae93@gmail.com>
2024-03-20 16:32:46 +0100

    feat: show empty board

--- src/main/java/com/codurance/TicTacToe.java
+++ src/main/java/com/codurance/TicTacToe.java
@@ -2,4 +2,7 @@ package com.codurance;
 
 public class TicTacToe {
 
+    public String showBoard() {
+        return "123456789";
+    }
 }
--- src/test/java/TicTacToeShould.java
+++ src/test/java/TicTacToeShould.java
@@ -1,10 +1,14 @@
 import com.codurance.TicTacToe;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class TicTacToeShould {
-    
+    @Test
+    public void show_empty_board() {
+        TicTacToe ticTacToe = new TicTacToe();
+        assertEquals("123456789", ticTacToe.showBoard());
+    }
+
 }
